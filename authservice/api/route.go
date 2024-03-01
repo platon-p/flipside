d7b6ed63e49f8924c2b26f24f4dabe0676aa73de
platon <platonpechenev@ya.ru>
2024-03-01 23:44:13 +0300

    auth & check routes

--- authservice/api/route.go
+++ authservice/api/route.go
@@ -2,23 +2,39 @@ package api
 
 import "github.com/gin-gonic/gin"
 
-func AddRoutes(group *gin.RouterGroup) {
-    api := group.Group("/api")
+func AddRoutes(group *gin.RouterGroup, authRouter AuthRouter, checkRouter CheckRouter) {
+	api := group.Group("/api")
 
-    auth := api.Group("/auth")
-    auth.POST("/register", RegisterHandler)
-    auth.POST("/login-by-email", LoginByEmailHandler)
-    auth.POST("/login-by-token", LoginByTokenHandler)
+	auth := api.Group("/auth")
+	auth.POST("/register", authRouter.Register)
+	auth.POST("/login-by-email", authRouter.LoginByEmail)
+	auth.POST("/login-by-token", authRouter.LoginByToken)
+
+	check := api.Group("/check")
+	check.GET("/email/:email", checkRouter.CheckEmail)
+	check.GET("/nickname/:nickname", checkRouter.CheckNickname)
 }
 
-func RegisterHandler(ctx *gin.Context) {
+type AuthRouter struct{}
+
+func (r *AuthRouter) Register(ctx *gin.Context) {
 
 }
 
-func LoginByEmailHandler(ctx *gin.Context) {
+func (r *AuthRouter) LoginByEmail(ctx *gin.Context) {
 
 }
 
-func LoginByTokenHandler(ctx *gin.Context) {
+func (r *AuthRouter) LoginByToken(ctx *gin.Context) {
+
+}
+
+type CheckRouter struct{}
+
+func (r *CheckRouter) CheckEmail(ctx *gin.Context) {
+
+}
+
+func (r *CheckRouter) CheckNickname(ctx *gin.Context) {
 
 }
