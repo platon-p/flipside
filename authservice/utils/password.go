c27980241b39de98cd4b38dddd7be7ee557c0d1d
platon <platonpechenev@ya.ru>
2024-03-02 00:37:58 +0300

    password util

---
+++ authservice/utils/password.go
@@ -0,0 +1,19 @@
+package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+type PasswordUtility struct {}
+
+func (u *PasswordUtility) GetPasswordHash(password string) (*string, error) {
+    hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+    if err != nil {
+        return nil, err
+    }
+    hashString := string(hash)
+    return &hashString, nil
+}
+
+func (u *PasswordUtility) CheckPasswordHash(hashedPassword, password string) bool {
+    err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+    return err != nil
+}
