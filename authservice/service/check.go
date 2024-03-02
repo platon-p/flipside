8e927125f6c4cc20c81b46a3c1ec62c6e997be41
platon <platonpechenev@ya.ru>
2024-03-02 22:52:55 +0300

    check service impl

--- authservice/service/check.go
+++ authservice/service/check.go
@@ -1,17 +1,35 @@
 package service
 
-import "errors"
+import (
+	"errors"
 
-var (
-    EmailIncorrectFormatError = errors.New("Incorrect format of email")
-    EmailExistsError = errors.New("Email already exists")
-
-    NicknameIncorrectFormatError = errors.New("Incorect format of nickname")
-    NicknameExistsError = errors.New("Nickname already exists")
+	"github.com/platon-p/flashside/authservice/repository"
 )
 
-type CheckService struct{}
+var (
+	EmailIncorrectFormatError = errors.New("Incorrect format of email")
+	EmailExistsError          = errors.New("Email already exists")
 
-func (s *CheckService) CheckEmail(email string) error
+	NicknameIncorrectFormatError = errors.New("Incorect format of nickname")
+	NicknameExistsError          = errors.New("Nickname already exists")
+)
 
-func (s *CheckService) CheckNickname(nickname string) error
+type CheckService struct {
+	userRepository repository.UserRepository
+}
+
+func (s *CheckService) CheckEmail(email string) error {
+    found := s.userRepository.FindByEmail(email)
+    if found == nil {
+        return nil
+    }
+    return EmailExistsError
+}
+
+func (s *CheckService) CheckNickname(nickname string) error {
+    found := s.userRepository.FindByNickname(nickname)
+    if found == nil {
+        return nil
+    }
+    return NicknameExistsError
+}
