34dac54b8043b8e44cba4f94707d3518504b3dcb
platon <platonpechenev@ya.ru>
2024-03-05 23:16:10 +0300

    cardset service

--- cardservice/service/cardset.go
+++ cardservice/service/cardset.go
@@ -1,22 +1,26 @@
 package service
 
-import "github.com/platon-p/flipside/cardservice/model"
+import (
+	"github.com/platon-p/flipside/cardservice/model"
+	"github.com/platon-p/flipside/cardservice/repository"
+)
 
 type CardSetService struct {
+	cardSetRepository repository.CardSetRepository
 }
 
-func (s *CardSetService) CreateCardSet(cardSet *model.CardSet) error {
-	panic("not implemented")
+func (s *CardSetService) CreateCardSet(cardSet *model.CardSet) (*model.CardSet, error) {
+	return s.cardSetRepository.CreateCardSet(cardSet)
 }
 
 func (s *CardSetService) GetCardSet(slug string) (*model.CardSet, error) {
-	panic("not implemented")
+	return s.cardSetRepository.GetCardSet(slug)
 }
 
-func (s *CardSetService) UpdateCardSet(cardSet *model.CardSet) error {
-	panic("not implemented")
+func (s *CardSetService) UpdateCardSet(cardSet *model.CardSet) (*model.CardSet, error) {
+	return s.cardSetRepository.UpdateCardSet(cardSet.Id, cardSet)
 }
 
 func (s *CardSetService) DeleteCardSet(cardSetId int) error {
-	panic("not implemented")
+	return s.cardSetRepository.DeleteCardSet(cardSetId)
 }
