b416759a435f0dbe9083459cc96db3a4c94792e8
platon <platonpechenev@ya.ru>
2024-03-05 23:12:22 +0300

    cardset route impl

--- cardservice/api/route/cardset.go
+++ cardservice/api/route/cardset.go
@@ -1,25 +1,83 @@
 package route
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/platon-p/flipside/cardservice/api/controller"
+	"github.com/platon-p/flipside/cardservice/api/helper"
+	"github.com/platon-p/flipside/cardservice/api/middleware"
 	"github.com/platon-p/flipside/cardservice/api/transfer"
 )
 
 type CardSetRouter struct {
-    controller *controller.CardSetController
+	controller     *controller.CardSetController
+	authMiddleware *middleware.AuthMiddleware
 }
 
 func (r *CardSetRouter) Setup(group *gin.RouterGroup) {
 	cardset := group.Group("/cardset")
 
-	cardset.POST("/", r.CreateCardSet)
 	cardset.GET("/:slug", r.GetCardSet)
-	cardset.PUT("/", r.UpdateCardSet)
-	cardset.DELETE("/:slug", r.DeleteCardSet)
+	cardset.Group("/").
+		Use(r.authMiddleware.Handler()).
+		POST("/", r.CreateCardSet).
+		PUT("/", r.UpdateCardSet).
+		DELETE("/:slug", r.DeleteCardSet)
 }
 
-func (r *CardSetRouter) CreateCardSet(ctx *gin.Context)
-func (r *CardSetRouter) GetCardSet(ctx *gin.Context)
-func (r *CardSetRouter) UpdateCardSet(ctx *gin.Context)
-func (r *CardSetRouter) DeleteCardSet(ctx *gin.Context)
+func (r *CardSetRouter) CreateCardSet(ctx *gin.Context) {
+	var request transfer.CardSetRequest
+	if err := ctx.BindJSON(&request); err != nil {
+		helper.ErrorMessage(ctx, http.StatusBadRequest, helper.BadRequest)
+		return
+	}
+	userId := ctx.GetInt("userId")
+	response, err := r.controller.CreateCardSet(userId, &request)
+	if err != nil {
+		fmt.Println("CreateCardSet:", err)
+		helper.ErrorMessage(ctx, http.StatusInternalServerError, "Internal server error")
+	} else {
+		ctx.JSON(http.StatusCreated, response)
+	}
+}
+
+func (r *CardSetRouter) GetCardSet(ctx *gin.Context) {
+	slug := ctx.Param("slug")
+	response, err := r.controller.GetCardSet(slug)
+	if err != nil {
+		fmt.Println("GetCardSet:", err)
+		helper.ErrorMessage(ctx, http.StatusInternalServerError, "Internal server error")
+	} else {
+		ctx.JSON(http.StatusOK, response)
+	}
+}
+
+func (r *CardSetRouter) UpdateCardSet(ctx *gin.Context) {
+	var request transfer.CardSetRequest
+	if err := ctx.BindJSON(&request); err != nil {
+		helper.ErrorMessage(ctx, http.StatusBadRequest, helper.BadRequest)
+		return
+	}
+	userId := ctx.GetInt("userId")
+	response, err := r.controller.UpdateCardSet(userId, &request)
+	if err != nil {
+		fmt.Println("UpdateCardSet:", err)
+		helper.ErrorMessage(ctx, http.StatusInternalServerError, "Internal server error")
+	} else {
+		ctx.JSON(http.StatusOK, response)
+	}
+
+}
+func (r *CardSetRouter) DeleteCardSet(ctx *gin.Context) {
+	slug := ctx.Param("slug")
+	userId := ctx.GetInt("userId")
+	err := r.controller.DeleteCardSet(userId, slug)
+	if err != nil {
+		fmt.Println("DeleteCardSet:", err)
+		helper.ErrorMessage(ctx, http.StatusInternalServerError, "Internal server error")
+	} else {
+		ctx.JSON(http.StatusNoContent, nil)
+	}
+}
