c5ef1ec1e320f698c9aaa011fdb9bee1d5df7db5
platon <platonpechenev@ya.ru>
2024-03-07 20:18:28 +0300

    card.controller impl

--- cardservice/api/controller/card.go
+++ cardservice/api/controller/card.go
@@ -21,45 +21,31 @@ func (c *CardController) CreateCards(
 	slug string,
 	request []transfer.CardRequest,
 ) ([]transfer.CardResponse, error) {
-    models := make([]model.Card, len(request))
-    for i, v := range request {
-        models[i] = model.Card{
-        	Question:  v.Question,
-        	Answer:    v.Answer,
-        	Position:  v.Position,
-        }
-    }
-    res, err := c.cardService.CreateCards(userId, slug, models)
-    if err != nil {
-        return nil, err
-    }
-    response := make([]transfer.CardResponse, len(res))
-    for i, v := range res {
-        response[i] = transfer.CardResponse{
-        	Question:  v.Question,
-        	Answer:    v.Answer,
-        	Position:  v.Position,
-        	CardSetId: v.CardSetId,
-        }
-    }
-    return response, nil
+	models := make([]model.Card, len(request))
+	for i, v := range request {
+		models[i] = *cardRequestToModel(&v)
+	}
+	res, err := c.cardService.CreateCards(userId, slug, models)
+	if err != nil {
+		return nil, err
+	}
+	response := make([]transfer.CardResponse, len(res))
+	for i, v := range res {
+		response[i] = *cardModelToResponse(&v)
+	}
+	return response, nil
 }
 
 func (c *CardController) GetCards(slug string) ([]transfer.CardResponse, error) {
-    res, err := c.cardService.GetCards(slug)
-    if err != nil {
-        return nil, err
-    }
-    response := make([]transfer.CardResponse, len(res))
-    for i, v := range res {
-        response[i] = transfer.CardResponse{
-        	Question:  v.Question,
-        	Answer:    v.Answer,
-        	Position:  v.Position,
-        	CardSetId: v.CardSetId,
-        }
-    }
-    return response, nil
+	res, err := c.cardService.GetCards(slug)
+	if err != nil {
+		return nil, err
+	}
+	response := make([]transfer.CardResponse, len(res))
+	for i, v := range res {
+		response[i] = *cardModelToResponse(&v)
+	}
+	return response, nil
 }
 
 func (c *CardController) UpdateCards(
@@ -67,9 +53,38 @@ func (c *CardController) UpdateCards(
 	slug string,
 	request []transfer.CardRequest,
 ) ([]transfer.CardResponse, error) {
-	panic("Not implemented")
+	models := make([]model.Card, len(request))
+	for i, v := range request {
+		models[i] = *cardRequestToModel(&v)
+	}
+	res, err := c.cardService.UpdateCards(userId, slug, models)
+	if err != nil {
+		return nil, err
+	}
+	response := make([]transfer.CardResponse, len(res))
+	for i, v := range res {
+		response[i] = *cardModelToResponse(&v)
+	}
+	return response, nil
 }
 
 func (c *CardController) DeleteCards(userId int, slug string, positions []string) error {
-	panic("Not implemented")
+	return c.DeleteCards(userId, slug, positions)
+}
+
+func cardRequestToModel(request *transfer.CardRequest) *model.Card {
+	return &model.Card{
+		Question: request.Question,
+		Answer:   request.Answer,
+		Position: request.Position,
+	}
+}
+
+func cardModelToResponse(card *model.Card) *transfer.CardResponse {
+	return &transfer.CardResponse{
+		Question:  card.Question,
+		Answer:    card.Answer,
+		Position:  card.Position,
+		CardSetId: card.CardSetId,
+	}
 }
