0c0c3472cef4bc6800668d3a4c207ba7722d82e3
platon <platonpechenev@ya.ru>
2024-03-09 01:07:30 +0300

    cardset page

--- frontend/src/pages/CardSet.tsx
+++ frontend/src/pages/CardSet.tsx
@@ -1,24 +1,77 @@
-import { useParams } from "react-router-dom"
-import { CardRepository } from "../repository/CardRepository";
-import { CardSetRepository } from "../repository/CardSetRepository";
+import { useNavigate, useParams } from "react-router-dom"
+import { CardSet as CardSetModel, CardSetRepository } from "../repository/CardSetRepository";
+import { useEffect, useState } from "react";
+import { Card, CardRepository } from "@/repository/CardRepository";
+import { useAuth } from "@/hooks/Auth";
+import { Button } from "@/components/Button";
 
 export function CardSet() {
+    const { userId } = useAuth();
     const { slug } = useParams();
-    const cardSet = CardSetRepository.getCardSetBySlug(slug!);
-    const cards = CardRepository.getCards(slug!);
+    const navigate = useNavigate();
 
-    function moveUp(position: number) {
+    const [cardSet, setCardSet] = useState<CardSetModel | undefined>();
+    const [cards, setCards] = useState<Card[] | undefined>();
+
+    const [loading, setLoading] = useState(true);
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+    useEffect(() => {
+        async function loadCardSet() {
+            try {
+                const cardSet = await CardSetRepository.getCardSetBySlug(slug!);
+                setCardSet(cardSet);
+                setLoading(false);
+            } catch (e) {
+                if (e === 'Card Set not found') {
+                    console.log('Card Set not found');
+                } else if (typeof e === 'string') {
+                    setErrorMessage(e);
+                } else {
+                    console.error(e);
+                }
+                setLoading(false);
+            }
+        }
+
+        async function loadCards() {
+            try {
+                const cards = await CardRepository.getCards(slug!);
+                setCards(cards);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+
+        Promise.all([loadCardSet(), loadCards()]);
+    }, [slug])
+
+    function goHome() {
+        navigate('/');
     }
 
-    function moveDown(position: number) {
+    if (errorMessage) {
+        return <p style={{
+            color: 'red'
+        }}>{errorMessage}</p>
+    }
+    if (loading) {
+        return <h2>Loading...</h2>
     }
 
     if (!cardSet) {
         return <h2>CardSet not found</h2>
     }
+
     return <div>
+        <p onClick={goHome}>home</p>
         <h2>{cardSet.title}</h2>
         <p>/{cardSet.slug}</p>
+
+        {cardSet.ownerId === userId && <div>
+            <Button>Edit</Button>
+            <Button>Delete</Button>
+        </div>}
 
         <h4>Cards</h4>
         <div style={{
@@ -26,7 +79,7 @@ export function CardSet() {
             flexDirection: 'column',
             gap: '1em'
         }}>
-            {cards.map((v, i) => {
+            {cards && cards.map((v, i) => {
                 return <div style={{
                     display: 'flex',
                     border: '1px solid black',
@@ -36,10 +89,9 @@ export function CardSet() {
                     <div style={{
                         display: 'flex',
                         flexDirection: 'column',
+                        justifyContent: 'center',
                     }}>
-                        <a onClick={() => moveUp(v.position)}>↑</a>
                         <a>#{v.position}</a>
-                        <a onClick={() => moveDown(v.position)}>↓</a>
                     </div>
                     <div style={{
                         display: 'flex',
