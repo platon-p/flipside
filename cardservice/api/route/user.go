5776f8c3da0b7d57d79f3ffb00725f2948405b90
platon <platonpechenev@ya.ru>
2024-03-09 13:37:32 +0300

    user router

---
+++ cardservice/api/route/user.go
@@ -0,0 +1,50 @@
+package route
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/platon-p/flipside/cardservice/api/controller"
+	"github.com/platon-p/flipside/cardservice/api/helper"
+	"github.com/platon-p/flipside/cardservice/service"
+)
+
+type UserRouter struct {
+    controller *controller.UserController
+}
+
+func (r *UserRouter) Setup(group *gin.RouterGroup) {
+    group.Group("/users").
+        GET("/:nickname/profile", r.GetProfileHandler).
+        GET("/:nickname/sets", r.GetSetsHandler)
+}
+
+func (r *UserRouter) GetProfileHandler(ctx *gin.Context) {
+    nickname := ctx.Param("nickname")
+    res, err := r.controller.GetProfile(nickname)
+    switch {
+    case errors.Is(err, service.ErrProfileNotFound):
+        helper.ErrorMessage(ctx, http.StatusNotFound, err.Error())
+    case err != nil:
+        fmt.Println("GetProfile:", err)
+        helper.ErrorMessage(ctx, http.StatusInternalServerError, "Internal server error")
+    default:
+        ctx.JSON(http.StatusOK, res)
+    }
+}
+
+func (r *UserRouter) GetSetsHandler(ctx *gin.Context) {
+    nickname := ctx.Param("nickname")
+    res, err := r.controller.GetSets(nickname)
+    switch {
+    case errors.Is(err, service.ErrProfileNotFound):
+        helper.ErrorMessage(ctx, http.StatusNotFound, err.Error())
+    case err != nil:
+        fmt.Println("GetSets:", err)
+        helper.ErrorMessage(ctx, http.StatusInternalServerError, "Internal server error")
+    default:
+        ctx.JSON(http.StatusOK, res)
+    }
+}
