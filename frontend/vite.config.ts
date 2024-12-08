de5522b220beaabb8badbdc82d1d62f19ef1f1ff
platon <platonpechenev@ya.ru>
2024-12-08 19:29:46 +0300

    postss.config.js -> vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite'
-import path from 'path';
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
+import tailwind from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind, autoprefixer],
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  }
-})
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
