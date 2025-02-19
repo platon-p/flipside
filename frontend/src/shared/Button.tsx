2cd6f9c49a52ccd6d8889be51073a01a53da01d6
platon <platonpechenev@ya.ru>
2025-02-19 22:53:13 +0300

    fix frontend

--- frontend/src/shared/Button.tsx
+++ frontend/src/shared/Button.tsx
@@ -5,3 +5,9 @@ export const Button = styled.button.attrs({
     "text-white rounded-[0.25rem] bg-primary " +
     "hover:bg-orange-600 duration-100 p-2",
 })``;
+
+export const LightButton = styled.button.attrs({
+  className:
+    `text-orange-600 font-medium rounded-[0.25rem] bg-orange-100
+    hover:bg-orange-300 hover:text-orange-900 duration-100 p-2`,
+})``;
